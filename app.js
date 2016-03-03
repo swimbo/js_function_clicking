c5525fbf3cb698a9275faa361a8dba0b0e53e0d3
Bo Bergstrom <bo.bergstrom@gmail.com>
2016-03-03 16:41:26 -0700

    another version of tic-tac-toe using OOD

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 (function() {
   'use strict';
   console.log('Tic Tac Toe')
-  //Create a player turn variable
-  var playerTurn = true
+
+
+
+
+  // ---------------------------------------------------------------------------
+  // Initialization - Add Click Listeners, Turn Toggle, Turn Counter, Reset Flag
+  // ---------------------------------------------------------------------------
+
   //Select all boxes with class box
   var boxes = document.querySelectorAll('.box')
-  // loop through the boxes and add click event listener
 
+  //Create a player turn variable
+  var playerTurn = true
 
-
+    // loop through the boxes and add click event listener
   for(var i = 0; i <  boxes.length; i++){
     boxes[i].addEventListener('click',changeBackgroundColor)
   }
@@ -32,8 +39,7 @@
   }
 
   //-------------------------------------------------------------------
-  // Win checking function that pushes all clicked boxes into an array and
-  // then checks whether or not they've won.
+  // Win checking function that pushes all clicked boxes into an array
   //-------------------------------------------------------------------
   function checkWinner(player){
       var playedBoxes = document.querySelectorAll('.' + player)
@@ -45,50 +51,72 @@
         console.log(playedBoxes[i])
         clicked.push(playedBoxes[i].id)
       }
-  }
       // Checking to see what this function is putting out:
       console.log(clicked)
 
-    //-------------------------------------------------------------------
-    // Listing out all the possible winning scenarios and setting them
-    // variables so I can easily reference them
-    //-------------------------------------------------------------------
 
-    var Win1 = ["box1", "box2", "box3"]
-    var Win2 = ["box4", "box5", "box6"]
-    var Win3 = ["box7", "box8", "box9"]
-    var Win4 = ["box1", "box4", "box7"]
-    var Win5 = ["box2", "box5", "box8"]
-    var Win6 = ["box3", "box6", "box9"]
-    var Win7 = ["box1", "box5", "box9"]
-    var Win8 = ["box3", "box2", "box6"]
 
-    //-------------------------------------------------------------------
-    // Checking each winning scenario to see if it matches the current state
-    //-------------------------------------------------------------------
+          //-------------------------------------------------------------------
+          // Listing out all the possible winning scenarios and setting them
+          // variables so I can easily reference them
+          //-------------------------------------------------------------------
 
-    function winloop()
+          var Win1 = ["box1", "box2", "box3"]
+          var Win2 = ["box4", "box5", "box6"]
+          var Win3 = ["box7", "box8", "box9"]
+          var Win4 = ["box1", "box4", "box7"]
+          var Win5 = ["box2", "box5", "box8"]
+          var Win6 = ["box3", "box6", "box9"]
+          var Win7 = ["box1", "box5", "box9"]
+          var Win8 = ["box3", "box2", "box6"]
 
-    //-------------------------------------------------------------------
-    // A function to iterate through the various Win options
-    //-------------------------------------------------------------------
+          //-------------------------------------------------------------------
+          // Checking each winning scenario to see if it matches the current state
+          //-------------------------------------------------------------------
 
-    // var ifelse = function(winloop)
-    // }
+          //-------------------------------------------------------------------
+          // A function to iterate through the various Win options
+          //-------------------------------------------------------------------
 
-    function winloop(){
-      for (var i = 1; i < 9, i++)
-      var winNumb = "Win" + i
-      console.log(playedBoxes[i])
-      if(clicked === "Win"+[i]){
-        alert(player + " wins!")
-      }
-      else  if(clicked === "Win"+(i+1)){
-          alert(player + " wins!")
-      }
-    }
+          // var ifelse = function(winloop)
+          // }
 
+            console.log(playedBoxes[i])
+
+
+            if(clicked === Win1){
+              alert(player + " wins!")
+            }
+            else  if(clicked === Win2){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win3){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win4){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win5){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win6){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win7){
+                alert(player + " wins!")
+            }
+            else  if(clicked === Win8){
+                alert(player + " wins!")
+            }
+            else  if(clicked.length < 9){
+
+            }
+            else{
+                alert("It's a tie")
+            }
 
   }
+
+
 
 }());
---
+++ app2.js
@@ -0,0 +1,95 @@
+var Game = {
+  // below are the properties of the game
+
+  // keeps track of the current players
+  players: [],
+  //
+  keeps track of the players turn
+  playerTurn: {},
+  //set the game board
+  board: [],
+  // games played
+  gamesPlayed: 0,
+  // counting the turns in that game
+  roundCount: 0,
+
+  //---------------------------------------------------------
+  // Now we'll start adding the methods
+  //---------------------------------------------------------
+
+  startGame: function(){
+    var player1 == new Player("X", "RebeccaPurple", "Jim")
+    //now we push that player into the players property of the game using .push
+    Game.players.push(player1)
+    var player1 == new Player("O", "black", "Bo")
+    Game.players.push(player2)
+    //now we need to set who goes first
+    Game.playerTurn = player1
+    //setup our game board
+    for (var i = 0; i < 9; i++)
+      Game.board.push(new Square(i))
+  }
+  changeTurn: function(){
+    Game.roundCount++
+    if(Game.playerTurn == Game.players[0]) {
+      Game.playerTurn = Game.players[1]
+    } else {
+      Game.playerTurn = Game.players[0]
+    }
+  }
+  //set game piece
+  setSquare: function(id){
+    Game.board[id].symbol = Game.playerTurn.symbol
+    Game.board[id].color = Game.playerTurn.color
+    Game.playerTurn.playerSquares.pickSquare(Game.board[id])
+  }
+  // finish game
+  endGame: function(){
+    Game.gamePlayed++
+    Game.newBoard()
+  }
+  checkWinner: function(){
+
+  }
+  newBoard: function(){
+    Game.board = []
+    //setup our game board
+    for (var i = 0; i < 9; i++)
+      Game.board.push(new Square(i))
+  }
+
+}
+
+//----------------------------------------------------------
+// Now we're outside of the game object
+// and we'll start adding the methods for a player
+// which will be in the form of a constructor function
+//---------------------------------------------------------
+
+function Player(symbol, color, name){
+  this.symbol = symbol
+  this.color = color
+  this.name = name
+  this.score = 0
+  this.playerSquares = []
+}
+
+//----------------------------------------------------------
+// Next, we'll add a method to our constructor function
+// and we'll start adding the methods for a player
+// which will be in the form of a constructor function
+//---------------------------------------------------------
+
+// format for this is as follows: Player.prototype.methodName = function (){}
+
+Player.prototype.methodName = function (square){
+  this.playerSquares.push(square)
+}
+
+// constructor function for a square
+
+function square(id){
+  this.symbol = ""
+  this.id = id
+  this.color = ""
+}
